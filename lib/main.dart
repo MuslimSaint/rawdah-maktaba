a7a92c730cb64abb1f2402358b9a8d1824fa96e4
Saint <amrimath69@gmail.com>
2026-06-29 13:54:45 +0300

    Initialize Flutter app with Arabic text

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('مكتبة الروضة'),
+        ),
+      ),
+    ),
+  );
+}
